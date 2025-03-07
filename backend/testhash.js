7b4c02db4e88b3ba9af1931b502cb9cdc0fe59af
yaji33 <yajii3835@gmail.com>
2025-03-07 20:39:21 +0800

    removed

--- backend/testhash.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-async function hashPassword() {
-  const hashed = await bcrypt.hash("password123", 10);
-  console.log("🔹 Hashed password:", hashed);
-}
-
-hashPassword();
