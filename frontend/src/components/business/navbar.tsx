a16f81be8181d5b3075e14479285f0e85cd79c68
yaji33 <yajii3835@gmail.com>
2025-03-26 00:14:10 +0800

    refactor

--- frontend/src/components/business/navbar.tsx
+++ frontend/src/components/business/navbar.tsx
@@ -1,36 +1,37 @@
 import React from "react";
 import Logo from "@/assets/logo.png";
 import { NavLink } from "react-router-dom";
+import ChatIcon from "@/assets/ep_chat-round.svg";
+import NotificationIcon from "@/assets/ion_notifications-outline.svg";
+import ShopIcon from "@/assets/mac&gab.svg";
 import "@/index.css";
 
 function navbar() {
   const navItems = [
     { to: "/business-dashboard", label: "Dashboard" },
-    { to: "/business-orders", label: "Users" },
-    { to: "/business-menu", label: "Eateries" },
+    { to: "/business-orders", label: "Orders" },
+    { to: "/business-menu", label: "Menu" },
     { to: "/business-transactions", label: "Transactions" },
   ];
 
   return (
-    <nav className="fixed top-0 w-full flex p-3 bg-white shadow-sm px-2 sm:px-8 z-50 justify-between">
-      <div className="w-full max-w-6xl mx-auto flex justify-between items-center">
+    <nav className="fixed top-0 w-full flex p-1 bg-white shadow-sm px-2 sm:px-8 z-50 justify-between">
+      <div className="w-full max-w-5xl mx-auto flex justify-between items-center">
         <div className="flex items-center">
           <img src={Logo} alt="logo" className="w-10" />
           <h1 className="font-bold text-2xl">
-            <span className="text-color_eat">Eat</span>
-            <span className="text-color_ease">Ease</span>
+            <span className="text-brandPrimary">Eat</span>
+            <span className="text-brandSecondary">Ease</span>
           </h1>
         </div>
-        <ul className="text-left list-none flex">
+        <ul className="text-left list-none flex text-md">
           {navItems.map((item) => (
             <li key={item.to}>
               <NavLink
                 to={item.to}
                 className={({ isActive }) =>
                   `flex items-center gap-3 px-3 py-2 text-black mx-2 rounded-md transition duration-300  ${
-                    isActive
-                      ? " text-text_active"
-                      : "hover:text-text_active "
+                    isActive ? " text-text_active" : "hover:text-text_active "
                   }`
                 }
               >
@@ -40,9 +41,15 @@ function navbar() {
           ))}
         </ul>
         <div className="flex gap-2">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
+          <button>
+            <img src={ChatIcon} alt="chat" className="w-4" />
+          </button>
+          <button>
+            <img src={NotificationIcon} alt="notification" className="w-4" />
+          </button>
+          <button>
+            <img src={ShopIcon} alt="shop" className="w-4" />
+          </button>
         </div>
       </div>
     </nav>
