266e4d92b43f04c6e130d5be8b383144834e7bbf
yaji33 <yajii3835@gmail.com>
2025-03-26 13:42:43 +0800

    build

--- backend/models/Role.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const RoleSchema = new mongoose.Schema({
-  id: { type: Number, required: true, unique: true }, 
-  name: { type: String, required: true, unique: true },
-});
-
-module.exports = mongoose.model("Role", RoleSchema);
