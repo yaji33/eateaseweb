fa8edf8a7d39beda8f794d52b1c51e00319f6f51
yaji33 <yajii3835@gmail.com>
2025-05-04 20:31:10 +0800

    fixed typescript errors

--- client/src/components/ui/loader.tsx
+++ client/src/components/ui/loader.tsx
@@ -1,5 +1,5 @@
 import { Loader2 } from "lucide-react"; 
-import React from "react";
+//import React from "react";
 
 export function Loader({ text = "Loading..." }: { text?: string }) {
   return (
